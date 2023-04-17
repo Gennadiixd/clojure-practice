a07bf1ab16ab3ed72894d9bad5aae71718175cdd
gennadiixd <gennadiixd@gmail.com>
2023-04-18 03:18:42 +0400

    update

--- gpt-tasks/index.clj
+++ gpt-tasks/index.clj
@@ -1,11 +1,18 @@
-;; students-over-credits: Write a function that takes the students list 
-;; and a credit threshold, and returns a list of student names who are 
-;; enrolled in more than the given number of credits.
+(require '[clj-http.client :as client])
+(require '[cheshire.core :as json])
 
-(def students
-  [{:name "Alice" :courses [{:id "CS101" :credits 3} {:id "MATH101" :credits 4}]}
-   {:name "Bob" :courses [{:id "CS101" :credits 3}]}
-   {:name "Charlie" :courses [{:id "MATH101" :credits 4} {:id "PHYS101" :credits 4}]}
-   {:name "David" :courses [{:id "CS101" :credits 3} {:id "PHYS101" :credits 4} {:id "MATH101" :credits 4}]}])
+(defn get-data [url]
+  (let [response (client/get url)]
+    (json/parse-string (:body response))))
 
-(prn (#(+ 6 %) 2))
+(def pokemon-api-url "https://pokeapi.co/api/v2/")
+(def get-pokemon-url (str pokemon-api-url "pokemon/"))
+
+(defn fetch-pokemon-by-name [pokemon-name] (get-data (str get-pokemon-url pokemon-name)))
+
+(defn display-pokemon-info [pokemon-name]
+  (let [pokemon (fetch-pokemon-by-name pokemon-name)
+        pokemon-types (map #(get-in % ["type" "name"]) (get pokemon "types"))]
+    (str "pokemon types are - " (reduce (fn [acc value] (str acc (if (empty? acc) "" ", ") value)) "" pokemon-types) ", pokemon name is - " (get pokemon "name") ", pokemon height is - " (get pokemon "height") ", pokemon weight is - " (get pokemon "weight") ", pokemon id is - " (get pokemon "id"))))
+
+(prn (display-pokemon-info "ditto"))
